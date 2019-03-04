f5656ad826f8915427632f60595b6b2722588be5
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2019-03-04 11:49:11 +0700

    example

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -6,7 +6,18 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("FisrtName: ________ ");
+            string firstName = Console.ReadLine();
+            Console.Write("BirthYear: ________ ");
+            string birthYear = Console.ReadLine();
+
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Your name is " + firstName);
+            Console.WriteLine("Your birthyear is " + birthYear);
+
+            int age = DateTime.Now.Year - int.Parse(birthYear);
+            Console.WriteLine("Your age is (age)" + age);
         }
     }
 }
