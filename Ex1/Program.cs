08c9beaf8833859fb45932ab1f638afe2c4fa3e2
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2019-03-04 12:00:24 +0700

    string template

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -18,6 +18,18 @@ namespace Ex1
 
             int age = DateTime.Now.Year - int.Parse(birthYear);
             Console.WriteLine("Your age is (age)" + age);
+
+
+            // Tip: string template
+
+            Console.WriteLine(" ------- string template --------- ");
+            Console.WriteLine("Your name is {0}", firstName);
+
+            string name = String.Format("Your name is {0}", firstName);
+
+            Console.WriteLine("C:\folder\file.txt"); // bad
+            Console.WriteLine("C:\\folder\\file.txt"); // good
+            Console.WriteLine(@"C:\folder\file.txt"); // good
         }
     }
 }
