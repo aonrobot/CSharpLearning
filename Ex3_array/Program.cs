19c0781337ae1616e669853d195773eb06ff0a10
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2019-03-04 15:33:36 +0700

    q3 array

--- Ex3_array/Program.cs
+++ Ex3_array/Program.cs
@@ -6,7 +6,21 @@ namespace Ex3_array
     {
         static void Main(string[] args)
         {
-            
+            // กรอกชื่อ 3 รอบ แล้วโชว์ชื่อ 3 ครั้ง
+
+            string[] names = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("First Name : ");
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("-----------------------------");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine("FirstName? is {0}", name);
+            }
         }
     }
 }
