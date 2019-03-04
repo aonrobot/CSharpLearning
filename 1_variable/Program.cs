f1c3854c17deb003e29090ae3bfe0aa793150965
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2019-03-04 10:36:55 +0700

    datatype

---
+++ 1_variable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Variable");
+
+            int a;
+            int c, d;
+
+            // Whole number
+            int integer; // 32bit
+            short shorter; // 16bit
+            long longer; // 64bit
+
+            uint unsign_integer;
+
+            // Decimal Number
+            float f;
+            f = 5.234F; // add suffix to convert double to float
+
+            double dou;
+            dou = 5.234;
+
+            decimal dec;
+            dec = 4.43211M;
+
+            // Boolean (true, false)
+            bool boo; boo = true;
+
+            // Character
+            char ch; ch = 'C';
+            string str; str = "String";
+
+        }
+    }
+}
